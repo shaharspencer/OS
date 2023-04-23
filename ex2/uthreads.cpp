98f44725c920e5e0805da19ea6b9c1bb2a4e0e51
n318162971 <nadav.eliash1@mail.huji.ac.il>
2023-04-24 01:17:01 +0300

    almost there

--- ex2/uthreads.cpp
+++ ex2/uthreads.cpp
@@ -3,54 +3,49 @@
 #include "uthreads.h"
 #include "Scheduler.h"
 
-static std::unique_ptr<Scheduler> scheduler; // TODO validate static use
+static Scheduler* scheduler; // TODO validate static use
 
 int uthread_init(int quantum_usecs) {
     if (quantum_usecs <= 0) {
         // TODO handle error
         return FAILURE;
     }
-    scheduler = std::make_unique<Scheduler>(quantum_usecs);
+    scheduler = new Scheduler(quantum_usecs);
     if (!scheduler) {
         // TODO handle malloc error
         return FAILURE;
     }
-    return create_main_thread(scheduler);
+    return SUCCESS;
 }
 
 int uthread_spawn(thread_entry_point entry_point) {
-    int tid = scheduler->get_free_tid();
-    std::shared_ptr <Thread> thread = std::make_shared<Thread>(tid, entry_point);
-    bool ok = scheduler->add_thread(main_thread);
-    if (!ok) {
-        // TODO handle error
-        return FAILURE;
-    }
-    return SUCCESS;
+    return scheduler->spawn(entry_point);
 }
 
 int uthread_terminate(int tid) {
-    // if we want to destroy the main process, just exit the program
-    if (tid == MAIN_TID){
-        delete scheduler;
-        exit(0);
-    }
-    if (!scheduler->terminate(tid)) {
-        // TODO handle error
-        return FAILURE;
-    }
-    return SUCCESS;
+    return scheduler->terminate(tid);
 }
 
 int uthread_block(int tid) {
-    bool ok = scheduler->block(tid);
-    if (!ok) {
-        // TODO handle error
-        return FAILURE;
-    }
-    return SUCCESS;
+    return scheduler->block(tid);
 }
 
 int uthread_resume(int tid) {
-    scheduler->resume(tid) ? SUCCESS : FAILURE;
+    return scheduler->resume(tid);
+}
+
+int uthread_sleep(int num_quantums) {
+    return scheduler->sleep(num_quantums);
+}
+
+int uthread_get_tid() {
+    return scheduler->get_running_thread();
+}
+
+int uthread_get_total_quantums() {
+    return scheduler->get_total_quanta_counter();
+}
+
+int uthread_get_quantums(int tid) {
+    return scheduler->get_quanta_counter(tid);
 }
